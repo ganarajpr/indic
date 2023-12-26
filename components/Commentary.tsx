b9b36f5d421a2c76a3646746bdcc59f1c8ab9e8e
Ganaraj Permunda <ganarajpr@gmail.com>
2023-12-26 21:31:20 +0000

    fixing issues with build

--- components/Commentary.tsx
+++ components/Commentary.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Commentary = ({ translations = [], className = '' }) => {
+const Commentary = ({ translations, className = '' }) => {
     return (
       <div className={`text-gray-500 text-2xl ${className}`}>
-        {translations.map((line: { commentary: string ; }, index: number) => (
+        {translations?.map((line: { commentary: string ; }, index: number) => (
           <div key={index} className="translation my-2">
             {line.commentary}
           </div>
