fef5bd8fb443eaf30fd3b114ee09a6a4dbaaee9d
Ganaraj Permunda <ganarajpr@gmail.com>
2024-01-10 12:47:12 +0000

    adding clarity

--- app/(withHeader)/layout.tsx
+++ app/(withHeader)/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,7 +9,6 @@ export const metadata = {
   description: 'Indic scriptures',
 };
 
-
 export default function BookLayout({
   children,
 }: {
@@ -16,15 +16,15 @@ export default function BookLayout({
 }) {
   return (
     <>
-        <Header />
-        {children}
-        <script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=G-YMJXKTXSRF'
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Header />
+      {children}
+      <script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-YMJXKTXSRF'
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -32,8 +32,20 @@ export default function BookLayout({
                     page_path: window.location.pathname,
                     });
                 `,
+        }}
+      />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kjnv3uen6k");
+        `,
           }}
         />
+      </Head>
     </>
   );
 }
